f7d70adacaee1c86577ac041aece348c228befc9
Judy Njeru <njerujudy87@ymail.com>
2020-02-19 19:00:33 +0100

    Initial project commit

---
+++ precache-manifest.d69ec950357de959a5effce08e835a35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "348af9736b9fd07505d6bd4d0917507e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bda2c5816d7d818dbb4",
+    "url": "/static/css/main.92a2970a.chunk.css"
+  },
+  {
+    "revision": "d09900fa683464508c96",
+    "url": "/static/js/2.f6c9c3a0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.f6c9c3a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bda2c5816d7d818dbb4",
+    "url": "/static/js/main.2df81547.chunk.js"
+  },
+  {
+    "revision": "5a2a762b724460524ae5",
+    "url": "/static/js/runtime-main.42a5c1ae.js"
+  }
+]);
